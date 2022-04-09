60dab08423cf6b77057801f14d8aca44c5c33ab8
Hien Luong <hyhien@gmail.com>
2022-04-09 13:20:24 +0700

    Add firestore config

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # niie-rn-firebase-19bitv02
+
 Demo React Native Firebase - 19BITV02
+
+# Cài thư viện
+
+npm i @react-navigation/native && npm i @react-navigation/stack && expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+npm i react-native-keyboard-aware-scroll-view base-64
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "apikey",
+    authDomain: "niie-rn-firebase-19bitv02.firebaseapp.com",
+    projectId: "niie-rn-firebase-19bitv02",
+    storageBucket: "niie-rn-firebase-19bitv02.appspot.com",
+    messagingSenderId: "msid",
+    appId: "appId"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
